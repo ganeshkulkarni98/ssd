92c4dffd9b78da53fbfff61d7f38df2f1098c43c
Ganesh Kulkarni <53935421+ganeshkulkarni98@users.noreply.github.com>
2020-06-09 12:47:29 +0530

    Update train.py

--- train.py
+++ train.py
@@ -5,6 +5,10 @@ import torch.utils.data
 from model import SSD300, MultiBoxLoss
 from datasets import PascalVOCDataset
 from utils import *
+from tqdm import tqdm
+from pprint import PrettyPrinter
+# Good formatting when printing the APs for each class and mAP
+pp = PrettyPrinter()
 
 # Data parameters
 #data_folder = './'  # folder with data files
@@ -13,6 +17,7 @@ keep_difficult = True  # use objects considered difficult to detect?
 # Model parameters
 # Not too many here since the SSD300 has a very specific structure
 n_classes = len(label_map)  # number of different types of objects
+print(n_classes)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Learning parameters
@@ -64,9 +69,18 @@ def main():
     model = model.to(device)
     criterion = MultiBoxLoss(priors_cxcy=model.priors_cxcy).to(device)
 
-    # Custom dataloaders
-
+    # Load test data
     create_data_lists('/content/data', '/content/data')
+
+    test_dataset = PascalVOCDataset('/content/data',
+                                    split='test',
+                                    keep_difficult=keep_difficult)                              
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False,
+                                              collate_fn=test_dataset.collate_fn, num_workers=workers, pin_memory=True)
+
+    # Custom train dataloaders
+
+    #create_data_lists('/content/data', '/content/data')
 
     train_dataset = PascalVOCDataset('/content/data',
                                      split='train',
@@ -84,6 +98,7 @@ def main():
     epochs = 10
     decay_lr_at = [it // (len(train_dataset) // 2) for it in decay_lr_at]
 
+    prev_mAP = 0.0
     # Epochs
     for epoch in range(start_epoch, epochs):
 
@@ -98,9 +113,15 @@ def main():
               optimizer=optimizer,
               epoch=epoch)
 
+        _, mAP = evaluate(test_loader, model)
+
         # Save checkpoint
         save_checkpoint(epoch, model, optimizer)
 
+        # save best checkpoint, having best mean average precision
+        if prev_mAP < mAP:
+          prev_mAP = mAP
+          save_best_checkpoint(epoch, model, optimizer)
 
 def train(train_loader, model, criterion, optimizer, epoch):
     """
@@ -161,6 +182,62 @@ def train(train_loader, model, criterion, optimizer, epoch):
                                                                   data_time=data_time, loss=losses))
     del predicted_locs, predicted_scores, images, boxes, labels  # free some memory since their histories may be stored
 
+def evaluate(test_loader, model):
+    """
+    Evaluate.
+
+    :param test_loader: DataLoader for test data
+    :param model: model
+    """
+    model = model.to(device)
+    # Make sure it's in eval mode
+
+    model.eval()
+
+    # Lists to store detected and true boxes, labels, scores
+    det_boxes = list()
+    det_labels = list()
+    det_scores = list()
+    true_boxes = list()
+    true_labels = list()
+    true_difficulties = list()  # it is necessary to know which objects are 'difficult', see 'calculate_mAP' in utils.py
+
+    with torch.no_grad():
+        # Batches
+        for i, (images, boxes, labels, difficulties) in enumerate(tqdm(test_loader, desc='Evaluating')):
+            images = images.to(device)  # (N, 3, 300, 300)
+
+            # Forward prop.
+            predicted_locs, predicted_scores = model(images)
+
+            # Detect objects in SSD output
+            det_boxes_batch, det_labels_batch, det_scores_batch = model.detect_objects(predicted_locs, predicted_scores,
+                                                                                       min_score=0.01, max_overlap=0.45,
+                                                                                       top_k=200)
+            # Evaluation MUST be at min_score=0.01, max_overlap=0.45, top_k=200 for fair comparision with the paper's results and other repos
+
+            # Store this batch's results for mAP calculation
+            boxes = [b.to(device) for b in boxes]
+            labels = [l.to(device) for l in labels]
+            difficulties = [d.to(device) for d in difficulties]
+
+            det_boxes.extend(det_boxes_batch)
+            det_labels.extend(det_labels_batch)
+            det_scores.extend(det_scores_batch)
+            true_boxes.extend(boxes)
+            true_labels.extend(labels)
+            true_difficulties.extend(difficulties)
+
+        # Calculate mAP
+        APs, mAP = calculate_mAP(det_boxes, det_labels, det_scores, true_boxes, true_labels, true_difficulties)
+
+    # Print AP for each class
+    print('Average Precision for each class : ')
+    pp.pprint(APs)
+
+    print('\nMean Average Precision (mAP): %.3f' % mAP)
+
+    return APs, mAP
 
 if __name__ == '__main__':
     main()
