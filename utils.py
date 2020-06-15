ed36d3928ef401b215e175f57c15680471f619a7
Ganesh Kulkarni <53935421+ganeshkulkarni98@users.noreply.github.com>
2020-06-16 01:32:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,24 +6,6 @@ import xml.etree.ElementTree as ET
 import torchvision.transforms.functional as FT
 import glob
 
-# #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# # Label map
-# # voc_labels = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
-# #               'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor')
-# voc_labels = ('fig', 'hazelnut','date')
-# label_map = {k: v + 1 for v, k in enumerate(voc_labels)}
-# label_map['background'] = 0
-# rev_label_map = {v: k for k, v in label_map.items()}  # Inverse mapping
-
-# # Color map for bounding boxes of detected objects from https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
-# distinct_colors = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231', '#911eb4', '#46f0f0', '#f032e6',
-#                   '#d2f53c', '#fabebe', '#008080', '#000080', '#aa6e28', '#fffac8', '#800000', '#aaffc3', '#808000',
-#                   '#ffd8b1', '#e6beff', '#808080', '#FFFFFF']
-# #distinct_colors=['#e6194b', '#3cb44b', '#ffe119']
-
-# label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())}
-
 def label_map_fn(json_file_path):
 
   anno = json.load(open(json_file_path))
@@ -37,138 +19,10 @@ def label_map_fn(json_file_path):
   distinct_colors = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231', '#911eb4', '#46f0f0', '#f032e6',
                     '#d2f53c', '#fabebe', '#008080', '#000080', '#aa6e28', '#fffac8', '#800000', '#aaffc3', '#808000',
                     '#ffd8b1', '#e6beff', '#808080', '#FFFFFF']
-  #distinct_colors=['#e6194b', '#3cb44b', '#ffe119']
 
-  label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())}
+  label_color_map = {k: random.choice(distinct_colors) for i, k in enumerate(label_map.keys())}
 
   return label_map, rev_label_map, label_color_map
-
-
-# def parse_annotation(annotation_path):
-#     tree = ET.parse(annotation_path)
-#     root = tree.getroot()
-
-#     boxes = list()
-#     labels = list()
-#     difficulties = list()
-#     for object in root.iter('object'):
-
-#         difficult = int(object.find('difficult').text == '1')
-
-#         label = object.find('name').text.lower().strip()
-#         if label not in label_map:
-#             continue
-
-#         bbox = object.find('bndbox')
-#         xmin = int(bbox.find('xmin').text) - 1
-#         ymin = int(bbox.find('ymin').text) - 1
-#         xmax = int(bbox.find('xmax').text) - 1
-#         ymax = int(bbox.find('ymax').text) - 1
-
-#         boxes.append([xmin, ymin, xmax, ymax])
-#         labels.append(label_map[label])
-#         difficulties.append(difficult)
-
-#     return {'boxes': boxes, 'labels': labels, 'difficulties': difficulties}
-
-
-# def create_data_lists(data_folder_path, output_folder):
-#     """
-#     Create lists of images, the bounding boxes and labels of the objects in these images, and save these to file.
-
-#     :param voc07_path: path to the 'VOC2007' folder
-#     :param voc12_path: path to the 'VOC2012' folder
-#     :param output_folder: folder where the JSONs must be saved
-#     """
-#     # voc07_path = os.path.abspath(voc07_path)
-#     # voc12_path = os.path.abspath(voc12_path)
-
-#     train_images = list()
-#     train_objects = list()
-#     n_objects = 0
-
-#     # Training data
-#     #for path in [voc07_path, voc12_path]:
-
-#         # Find IDs of images in training data
-#         # with open(os.path.join(path, 'ImageSets/Main/trainval.txt')) as f:
-#         #     ids = f.read().splitlines()
-#     lables_files = glob.glob1(os.path.join(data_folder_path,'Annotations'), '*.xml')
-#     for label in lables_files:
-#       label = os.path.join(data_folder_path,'Annotations', label)
-#       objects = parse_annotation(label)
-#       if len(objects) == 0:
-#           continue      
-#       n_objects += len(objects)
-#       train_objects.append(objects)
-
-#     for image in glob.glob(os.path.join(data_folder_path,'images') + '/*'):
-#       train_images.append(image)
-#     #train_images = glob.glob1(os.path.join(data_folder_path,'images'), '*.jpg')
-
-#         # for id in ids:
-#         #     # Parse annotation's XML file
-#         #     objects = parse_annotation(os.path.join(path, 'Annotations', id + '.xml'))
-#         #     if len(objects) == 0:
-#         #         continue
-#         #     n_objects += len(objects)
-#         #     train_objects.append(objects)
-#         #     train_images.append(os.path.join(path, 'JPEGImages', id + '.jpg'))
-
-#     assert len(train_objects) == len(train_images)
-
-#     # Save to file
-#     with open(os.path.join(output_folder, 'TRAIN_images.json'), 'w') as j:
-#         json.dump(train_images, j)
-#     with open(os.path.join(output_folder, 'TRAIN_objects.json'), 'w') as j:
-#         json.dump(train_objects, j)
-#     with open(os.path.join(output_folder, 'label_map.json'), 'w') as j:
-#         json.dump(label_map, j)  # save label map too
-
-#     print('\nThere are %d training images containing a total of %d objects. Files have been saved to %s.' % (
-#         len(train_images), n_objects, os.path.abspath(output_folder)))
-
-
-#     # Test data
-#     test_images = list()
-#     test_objects = list()
-#     n_objects = 0
-
-# #     # Find IDs of images in the test data
-# #     with open(os.path.join(voc07_path, 'ImageSets/Main/test.txt')) as f:
-# #         ids = f.read().splitlines()
-
-# #     for id in ids:
-# #         # Parse annotation's XML file
-# #         objects = parse_annotation(os.path.join(voc07_path, 'Annotations', id + '.xml'))
-# #         if len(objects) == 0:
-# #             continue
-# #         test_objects.append(objects)
-# #         n_objects += len(objects)
-# #         test_images.append(os.path.join(voc07_path, 'JPEGImages', id + '.jpg'))
-#     lables_files = glob.glob1(os.path.join(data_folder_path,'Annotations'), '*.xml')
-#     for label in lables_files:
-#       label = os.path.join(data_folder_path,'Annotations', label)
-#       objects = parse_annotation(label)
-#       if len(objects) == 0:
-#           continue      
-#       n_objects += len(objects)
-#       test_objects.append(objects)
-
-#     for image in glob.glob(os.path.join(data_folder_path,'images') + '/*'):
-#       test_images.append(image)
-      
-#     assert len(test_objects) == len(test_images)
-
-#     # Save to file
-#     with open(os.path.join(output_folder, 'TEST_images.json'), 'w') as j:
-#         json.dump(test_images, j)
-#     with open(os.path.join(output_folder, 'TEST_objects.json'), 'w') as j:
-#         json.dump(test_objects, j)
-
-#     print('\nThere are %d test images containing a total of %d objects. Files have been saved to %s.' % (
-#         len(test_images), n_objects, os.path.abspath(output_folder)))
-
 
 def decimate(tensor, m):
     """
