25104591284c4b6522a2120f171e631a2595930b
Ganesh Kulkarni <53935421+ganeshkulkarni98@users.noreply.github.com>
2020-06-19 19:19:17 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -11,8 +11,9 @@ def model_init(model_name):
     As in the paper, we use a VGG-16 pretrained on the ImageNet task as the base network.
     There's one available in PyTorch, see https://pytorch.org/docs/stable/torchvision/models.html#torchvision.models.vgg16 
     '''
-    weight_file_path = '/content/ssd/vgg16-397923af.pth'
+    #weight_file_path = '/content/ssd/vgg16-397923af.pth'
     #weight_file_path = '/content/ssd/CP_epoch1.pth'
+    weight_file_path = '/content/ssd/SSD300_pretrained_weight.pth'
 
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
   print('Loading model')
@@ -56,80 +57,83 @@ def detect(image_folder, min_score, max_overlap, top_k, suppress=None):
     annotated_images = []
     results = []
 
-    for original_image in images:
-      img_result = []
-      original_image = Image.open(os.path.join(image_folder, original_image)).convert("RGB")
+    with torch.no_grad():
+      for original_image in images:
+        img_result = []
+        #annotated_image_path = '/content/results/' + original_image
+        original_image = Image.open(os.path.join(image_folder, original_image)).convert("RGB")
 
-      # Transform
-      image = normalize(to_tensor(resize(original_image)))
+        # Transform
+        image = normalize(to_tensor(resize(original_image)))
 
-      # Move to default device
-      image = image.to(device)
+        # Move to default device
+        image = image.to(device)
 
-      # Forward prop.
-      predicted_locs, predicted_scores = model(image.unsqueeze(0))
+        # Forward prop.
+        predicted_locs, predicted_scores = model(image.unsqueeze(0))
 
-      # Detect objects in SSD output
-      det_boxes, det_labels, det_scores = model.detect_objects(predicted_locs, predicted_scores, min_score=min_score,
-                                                              max_overlap=max_overlap, top_k=top_k)
+        # Detect objects in SSD output
+        det_boxes, det_labels, det_scores = model.detect_objects(predicted_locs, predicted_scores, min_score=min_score,
+                                                                max_overlap=max_overlap, top_k=top_k)
 
-      # Move detections to the CPU
-      det_boxes = det_boxes[0].to('cpu')
-      #print(det_scores)
-      # Transform to original image dimensions
-      original_dims = torch.FloatTensor(
-          [original_image.width, original_image.height, original_image.width, original_image.height]).unsqueeze(0)
-      det_boxes = det_boxes * original_dims
+        # Move detections to the CPU
+        det_boxes = det_boxes[0].to('cpu')
+        #print(det_scores)
+        # Transform to original image dimensions
+        original_dims = torch.FloatTensor(
+            [original_image.width, original_image.height, original_image.width, original_image.height]).unsqueeze(0)
+        det_boxes = det_boxes * original_dims
 
-      # Decode class integer labels
-      det_labels = [rev_label_map[l] for l in det_labels[0].to('cpu').tolist()]
+        # Decode class integer labels
+        det_labels = [rev_label_map[l] for l in det_labels[0].to('cpu').tolist()]
 
-      # If no objects found, the detected labels will be set to ['0.'], i.e. ['background'] in SSD300.detect_objects() in model.py
-      if det_labels == ['background']:
-          # Just return original image
-          
-        annotated_images.append(np.array(image.cpu()))
-        results.append([])
+        # If no objects found, the detected labels will be set to ['0.'], i.e. ['background'] in SSD300.detect_objects() in model.py
+        if det_labels == ['background']:
+            # Just return original image
+            
+          annotated_images.append(np.array(image.cpu()))
+          results.append([])
 
-      else:      
-        # Annotate
-        annotated_image = original_image
-        draw = ImageDraw.Draw(annotated_image)
-        font = ImageFont.load_default()
+        else:      
+          # Annotate
+          annotated_image = original_image
+          draw = ImageDraw.Draw(annotated_image)
+          font = ImageFont.load_default()
 
-        # Suppress specific classes, if needed
-        for i in range(det_boxes.size(0)):
-            objects = {}
-            objects['x_min'] = det_boxes[i][0]
-            objects['y_min'] = det_boxes[i][1]
-            objects['x_max'] = det_boxes[i][2]
-            objects['y_max'] = det_boxes[i][3]
-            objects['conf_level'] = det_scores[0][i]
-            objects['label']= det_labels[i]
-            if suppress is not None:
-                if det_labels[i] in suppress:
-                    continue
+          # Suppress specific classes, if needed
+          for i in range(det_boxes.size(0)):
+              objects = {}
+              objects['x_min'] = det_boxes[i][0]
+              objects['y_min'] = det_boxes[i][1]
+              objects['x_max'] = det_boxes[i][2]
+              objects['y_max'] = det_boxes[i][3]
+              objects['conf_level'] = det_scores[0][i]
+              objects['label']= det_labels[i]
+              if suppress is not None:
+                  if det_labels[i] in suppress:
+                      continue
 
-            img_result.append(objects)
+              img_result.append(objects)
 
-            # Boxes
-            box_location = det_boxes[i].tolist()
-            draw.rectangle(xy=box_location, outline=label_color_map[det_labels[i]])
-            draw.rectangle(xy=[l + 1. for l in box_location], outline=label_color_map[
-                det_labels[i]]) 
+              # Boxes
+              box_location = det_boxes[i].tolist()
+              draw.rectangle(xy=box_location, outline=label_color_map[det_labels[i]])
+              draw.rectangle(xy=[l + 1. for l in box_location], outline=label_color_map[
+                  det_labels[i]]) 
 
-            # Text
-            text_size = font.getsize(det_labels[i].upper())
-            text_location = [box_location[0] + 2., box_location[1] - text_size[1]]
-            textbox_location = [box_location[0], box_location[1] - text_size[1], box_location[0] + text_size[0] + 4.,
-                                box_location[1]]
-            draw.rectangle(xy=textbox_location, fill=label_color_map[det_labels[i]])
-            draw.text(xy=text_location, text=det_labels[i].upper(), fill='white',
-                      font=font)
-        del draw
+              # Text
+              text_size = font.getsize(det_labels[i].upper())
+              text_location = [box_location[0] + 2., box_location[1] - text_size[1]]
+              textbox_location = [box_location[0], box_location[1] - text_size[1], box_location[0] + text_size[0] + 4.,
+                                  box_location[1]]
+              draw.rectangle(xy=textbox_location, fill=label_color_map[det_labels[i]])
+              draw.text(xy=text_location, text=det_labels[i].upper(), fill='white',
+                        font=font)
+          del draw
 
-        annotated_images.append(np.array(annotated_image))
-        results.append(img_result)
+          # annotated_image.save(annotated_image_path)
+          annotated_images.append(np.array(annotated_image))
+          results.append(img_result)
 
     return annotated_images, results
 
@@ -137,19 +141,23 @@ def detect(image_folder, min_score, max_overlap, top_k, suppress=None):
 if __name__ == '__main__':
 
     # hyperparameters
-    min_score=0.01
+    min_score=0.8
     max_overlap=0.45
-    top_k=200
+    top_k=5
 
-    label_map, rev_label_map, label_color_map = label_map_fn('/content/data/output.json')
+    model_name = 'SSD'
+
+    test_json_file = '/content/test_coco_dataset.json'
+    test_images_folder = '/content/test_images'
+
+    label_map, rev_label_map, label_color_map = label_map_fn(train_json_file)
 
     n_classes = len(label_map)  # number of different types of objects
 
-    model, device = model_init('SSD')
+    model, device = model_init(model_name)
     model = model.to(device)
+    # Switch to eval mode
     model.eval()
-
-    image_folder = '/content/data/images'
 
     annotated_images, results = detect(image_folder, min_score, max_overlap, top_k)
     print(annotated_images, results)
